174ee74d0d6d64ed58e1286d15a88cc955b8f300
selfup <boudinot.regis@yahoo.com>
2015-10-26 11:13:53 -0600

    works and tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  get 'auth/twitter', as: :login
+  get 'auth/twitter/callback' => 'sessions#create'
+  get "/logout" => 'sessions#destroy'
 end
