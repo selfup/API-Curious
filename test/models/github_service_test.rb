88ba58e2ef9289c825401427b9f428080cfb887a
selfup <boudinot.regis@yahoo.com>
2015-10-28 13:18:52 -0600

    added all service tests

--- test/models/github_service_test.rb
+++ test/models/github_service_test.rb
@@ -4,7 +4,7 @@ class GithubServiceTest < ActiveSupport::TestCase
   attr_reader :service, :user
 
   def setup
-    @user = User.create(nickname: "selfup", uid: "github", token: ENV["TOKEN"])
+    @user ||= User.create(nickname: "selfup", uid: "github", token: ENV["TOKEN"])
     @service ||= GithubService.new(user)
   end
 
@@ -15,6 +15,34 @@ class GithubServiceTest < ActiveSupport::TestCase
 
       assert_equal 28, repos.count
       assert_equal 'API-Curious', repo["name"]
+    end
+  end
+
+  test '#followers' do
+    VCR.use_cassette("github_service#followers") do
+      followers = service.followers(user)
+      follower = service.followers(user).first
+
+      assert_equal 25, followers.count
+      assert_equal 'Abouttabs', follower["login"]
+    end
+  end
+
+  test '#organizations' do
+    VCR.use_cassette("github_service#organizations") do
+      organizations = service.organizations(user)
+      organization = service.organizations(user).first
+
+      assert_equal 0, organizations.count
+    end
+  end
+
+  test '#pull_request_count' do
+    VCR.use_cassette("github_service#pull_request_count") do
+      pull_request_count = service.pull_request_count(user)
+
+      assert_equal 0, pull_request_count.first
+      assert_equal 28, pull_request_count.count
     end
   end
 end
