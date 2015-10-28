d9b29a26fa18abd081d9dc0c937c81c463598632
selfup <boudinot.regis@yahoo.com>
2015-10-27 21:16:35 -0600

    fixes locally

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @current_streak = Github.new(current_user).current_streak
-    @longest_streak = Github.new(current_user).longest_streak
-    @year_to_date   = Github.new(current_user).year_to_date
+    if current_user == nil
+      current_user
+    else
+      @current_streak = Github.new(current_user).current_streak
+      @longest_streak = Github.new(current_user).longest_streak
+      @year_to_date   = Github.new(current_user).year_to_date
+    end
   end
 end
