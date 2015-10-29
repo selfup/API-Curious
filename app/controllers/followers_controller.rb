8cbe22531368955ed0447c2fc019c1c1ca9056a5
selfup <boudinot.regis@yahoo.com>
2015-10-29 10:59:11 -0600

    removed commented out code

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,7 +1,5 @@
 class FollowersController < ApplicationController
   def index
     @show = FollowerDecorator.new(current_user)
-    # @followers = Github.new(current_user).all_followers
-    # @follower_events = Github.new(current_user).latest_followers_events
   end
 end
