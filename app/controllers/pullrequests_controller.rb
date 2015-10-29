1f4cfe597287608cd82021b1b9d9a6ba1ec06d86
selfup <boudinot.regis@yahoo.com>
2015-10-29 10:43:07 -0600

    decorated three controllers

--- app/controllers/pullrequests_controller.rb
+++ app/controllers/pullrequests_controller.rb
@@ -1,7 +1,5 @@
 class PullrequestsController < ApplicationController
   def index
     @show = PullrequestDecorator.new(current_user)
-    # @repos         = Github.new(current_user).all_repos
-    # @pull_requests = Github.new(current_user).pull_requests
   end
 end
